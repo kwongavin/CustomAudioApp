d8078a76441db6a524bb391b334d53bc3be00122
Gavin Kwon <kwongavin@gmail.com>
2023-06-08 16:20:55 -0700

    Initial Commit

---
+++ UserAudioApp_0608/AudioTrackView.swift
@@ -0,0 +1,20 @@
+//
+//  AudioTrackView.swift
+//  UserAudioApp_0608
+//
+//  Created by Gavin Kwon on 6/8/23.
+//
+
+import SwiftUI
+
+struct AudioTrackView: View {
+    var body: some View {
+        Text("Hey")
+    }
+}
+
+struct AudioTrackView_Previews: PreviewProvider {
+    static var previews: some View {
+        AudioTrackView()
+    }
+}
---
+++ UserAudioApp_0608/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  UserAudioApp_0608
+//
+//  Created by Gavin Kwon on 6/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
