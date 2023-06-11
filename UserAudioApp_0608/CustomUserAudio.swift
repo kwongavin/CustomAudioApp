9b2c08e6dbb7da7e3df5a594294702ecb821fae5
Sibtain Ali <sibtain@satl.me>
2023-06-11 16:28:34 +0500

    -

---
+++ UserAudioApp_0608/CustomUserAudio.swift
@@ -0,0 +1,17 @@
+//
+//  UserAudioApp_0608App.swift
+//  UserAudioApp_0608
+//
+//  Created by Gavin Kwon on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomUserAudio: App {
+    var body: some Scene {
+        WindowGroup {
+            AudioTrackView()
+        }
+    }
+}
